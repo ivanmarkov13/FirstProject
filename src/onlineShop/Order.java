ade790a827f9edf67405b1852115152da0cd1412
Zara Andreeva <zara.andreeva@yahoo.com>
2017-03-05 23:26:00 +0200

    added constructor, hashCode and equals and registrateUser() in class VzemiMe

--- src/onlineShop/Order.java
+++ src/onlineShop/Order.java
@@ -12,8 +12,19 @@ public class Order {
 	private User user;
 	private boolean isVerified;
 	
-	public void verifyOrder(){
-		//sending an email
+	public Order(HashMap<Product, Integer> products, Date date, User user) {
+		this.products = new HashMap<>();
+		this.products = products;
+		this.date = date;
+		this.user = user;
+		if(verifyOrder()){
+			this.isVerified = true;
+		}
+	}
+	
+	public boolean verifyOrder(){
+		//TODO sending an email
+		return false;
 	}
 	
 
