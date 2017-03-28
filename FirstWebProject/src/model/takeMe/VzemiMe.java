92973a8d8f9cfaa217d9931b1f941bf445bb530a
Ivan Markov <ivan.markov13@yahoo.com>
2017-03-28 21:27:41 +0300

    added ProductDAO

--- FirstWebProject/src/model/takeMe/VzemiMe.java
+++ FirstWebProject/src/model/takeMe/VzemiMe.java
@@ -12,113 +12,113 @@ import onlineShop.Product.ProductCategory;
 import users.User;
 
 public class VzemiMe {
-	
-	
-	
-//	private TreeSet<Product> productsByName;
-	private HashMap<ProductCategory, TreeSet<Product>> products;
-	private TreeSet<User> users;
-	private HashSet<Order> finishedOrders;
-	private static VzemiMe instance;
-	
-	private VzemiMe() {
-		
-//		productsByName = new TreeSet<>(new Comparator<Product>() {
+//	
+//	
+//	
+////	private TreeSet<Product> productsByName;
+//	private HashMap<ProductCategory, TreeSet<Product>> products;
+//	private TreeSet<User> users;
+//	private HashSet<Order> finishedOrders;
+//	private static VzemiMe instance;
+//	
+//	private VzemiMe() {
+//		
+////		productsByName = new TreeSet<>(new Comparator<Product>() {
+////			@Override
+////			public int compare(Product o1, Product o2) {
+////				return o1.getName().compareTo(o2.getName());
+////			}
+////		});
+//		
+//		this.products = new  HashMap<>();
+//		ProductComparatorByRating comp = new ProductComparatorByRating();
+//		this.products.put(ProductCategory.BEAUTY, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.SPA, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.RELAX, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.FOOD, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.ADVENTURE, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.SECOND_HAND, new TreeSet<>(comp));
+//		this.products.put(ProductCategory.BRAND_NEW, new TreeSet<>(comp));
+//		
+//		this.users = new TreeSet<>(new Comparator<User>() {
+//
 //			@Override
-//			public int compare(Product o1, Product o2) {
-//				return o1.getName().compareTo(o2.getName());
+//			public int compare(User o1, User o2) {
+//				return o1.getUsername().compareTo(o2.getUsername());
 //			}
+//			
 //		});
-		
-		this.products = new  HashMap<>();
-		ProductComparatorByRating comp = new ProductComparatorByRating();
-		this.products.put(ProductCategory.BEAUTY, new TreeSet<>(comp));
-		this.products.put(ProductCategory.SPA, new TreeSet<>(comp));
-		this.products.put(ProductCategory.RELAX, new TreeSet<>(comp));
-		this.products.put(ProductCategory.FOOD, new TreeSet<>(comp));
-		this.products.put(ProductCategory.ADVENTURE, new TreeSet<>(comp));
-		this.products.put(ProductCategory.SECOND_HAND, new TreeSet<>(comp));
-		this.products.put(ProductCategory.BRAND_NEW, new TreeSet<>(comp));
-		
-		this.users = new TreeSet<>(new Comparator<User>() {
-
-			@Override
-			public int compare(User o1, User o2) {
-				return o1.getUsername().compareTo(o2.getUsername());
-			}
-			
-		});
-		
-		this.finishedOrders = new HashSet<>();
-	}
-	
-	public static VzemiMe getInstance(){
-		if(instance == null){
-			instance = new VzemiMe();
-		}
-		return instance;
-	}
-	
-	public void addOrder(Order o){
-		
-	}
-	
-	public void registerUser(User u){
-		if(users.contains(u)){
-			System.out.println("The user already has a registration.");
-			return;
-		}
-		users.add(u);
-	}
-
-	public void addFinishedOrder(Order o){
-		if(o == null){
-			return;
-		}
-		this.finishedOrders.add(o);
-		
-	}
-	
-	
-	public void addProduct(Product p){
-		if(p == null){
-			return;
-		}
-		
-		HashSet<ProductCategory> categories = p.getCategories();
-		
-		for (ProductCategory productCategory : categories) {
-			this.products.get(productCategory).add(p);
-		}
-		
-		
-	}
-	
-	private void putProductsInSortedCollection(TreeSet<Product> sortedProducts){
-		for (Entry<ProductCategory, TreeSet<Product>> e : this.products.entrySet()) {
-			for (Product product : e.getValue()) {
-				sortedProducts.add(product);
-			}
-		}
-	}
-	
-	
-	public void sortAllProducts(IProductComparator comp){
-		TreeSet<Product> sortedProducts = new TreeSet<>(comp);
-		putProductsInSortedCollection(sortedProducts);
-		
-		//TODO print sortedProducts
-	}
-	
-	
-	public void sortCategory(ProductCategory category, IProductComparator comp){
-		TreeSet<Product> sortedCategory = new TreeSet<>(comp);
-		
-		for (Product product : this.products.get(category)) {
-			sortedCategory.add(product);
-		}
-		
-		//TODO something with the sorted category
-	}
+//		
+//		this.finishedOrders = new HashSet<>();
+//	}
+//	
+//	public static VzemiMe getInstance(){
+//		if(instance == null){
+//			instance = new VzemiMe();
+//		}
+//		return instance;
+//	}
+//	
+//	public void addOrder(Order o){
+//		
+//	}
+//	
+//	public void registerUser(User u){
+//		if(users.contains(u)){
+//			System.out.println("The user already has a registration.");
+//			return;
+//		}
+//		users.add(u);
+//	}
+//
+//	public void addFinishedOrder(Order o){
+//		if(o == null){
+//			return;
+//		}
+//		this.finishedOrders.add(o);
+//		
+//	}
+//	
+//	
+//	public void addProduct(Product p){
+//		if(p == null){
+//			return;
+//		}
+//		
+//		HashSet<ProductCategory> categories = p.getCategories();
+//		
+//		for (ProductCategory productCategory : categories) {
+//			this.products.get(productCategory).add(p);
+//		}
+//		
+//		
+//	}
+//	
+//	private void putProductsInSortedCollection(TreeSet<Product> sortedProducts){
+//		for (Entry<ProductCategory, TreeSet<Product>> e : this.products.entrySet()) {
+//			for (Product product : e.getValue()) {
+//				sortedProducts.add(product);
+//			}
+//		}
+//	}
+//	
+//	
+//	public void sortAllProducts(IProductComparator comp){
+//		TreeSet<Product> sortedProducts = new TreeSet<>(comp);
+//		putProductsInSortedCollection(sortedProducts);
+//		
+//		//TODO print sortedProducts
+//	}
+//	
+//	
+//	public void sortCategory(ProductCategory category, IProductComparator comp){
+//		TreeSet<Product> sortedCategory = new TreeSet<>(comp);
+//		
+//		for (Product product : this.products.get(category)) {
+//			sortedCategory.add(product);
+//		}
+//		
+//		//TODO something with the sorted category
+//	}
 	
 }
